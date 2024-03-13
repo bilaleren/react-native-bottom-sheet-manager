a787a8b81436c6815ec9a72abb806a0a30ecd523
webnoi <bilal@webnoi.com>
2024-03-13 23:13:35 +0300

    feat: initial commit

--- src/utils/warnIfIsDynamicValue.ts
+++
@@ -1,9 +0,0 @@
-function warnIfIsDynamicValue(a: unknown, b: unknown, paramName: string): void {
-  if (a !== b) {
-    console.warn(
-      `The "${paramName}" parameter cannot be provided a dynamic value.`
-    );
-  }
-}
-
-export default warnIfIsDynamicValue;
